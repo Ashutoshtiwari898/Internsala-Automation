d5cfd705545266131fb1547f34a9758aa4924c29
Ashutosh Kumar Tiwari <9895145@gmail.com>
2024-07-28 08:58:54 +0530

    Login page

--- Internshala.js
+++ Internshala.js
@@ -19,4 +19,19 @@ browserOpen
     page = newTab;
     let internShalaOpenPromise = newTab.goto(loginLink); // Go to login page
     return internShalaOpenPromise;
-  });
+  })
+  .then(function () {
+    return page.type("input[id='email']", process.env.EMAIL, { delay: 50 }); // Type email
+  })
+  .then(function () {
+    return page.type("input[id='password']", process.env.PASSWORD, {
+      delay: 50,
+    }); // Type password
+  })
+  .then(function () {
+    return page.click('button[id="login_submit"]', { delay: 40 }); // Click login button
+  })
+  .then(function () {
+    return page.waitForNavigation(); // Wait for navigation after login
+  })
+  
